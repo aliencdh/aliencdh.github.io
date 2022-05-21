cb9f4fe9654dacba63fa127658576e598b886b4a
aliencdh <cdevhax@gmail.com>
2022-05-21 23:03:24 +0200

    initialized svelte project

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import NavBar from './NavBar.svelte';
+	let name = 'world';
+</script>
+
+<div class="main">
+	<h1>Hello {name}!</h1>	
+</div>
+
+
+<style>
+	.main {
+		color: white;
+		background-color: #292929;
+		height: 100%;
+		width: 100%;
+		justify-content: center;
+		display: flex;
+		font-family: 'Inconsolata', monospace;
+	}
+</style>

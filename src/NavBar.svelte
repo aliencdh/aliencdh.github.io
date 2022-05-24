ff62486f389303c23d048a8505bb945200a2cdd9
aliencdh <cdevhax@gmail.com>
2022-05-24 22:12:43 +0200

    added about me and languages sections

--- src/NavBar.svelte
+++ src/NavBar.svelte
@@ -1,3 +1,18 @@
-<script>
-</script>
+<div class="nav">
+    <a href="/"><h3>home</h3></a>
+</div>
 
+<style>
+    .nav {
+		justify-content: center;
+		display: flex;
+
+		font-family: 'Inconsolata', monospace;
+        font-size: medium;
+    }
+
+    .nav a {
+        color: white;
+        text-decoration: none;
+    }
+</style>
---
+++ src/Section.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let settings = {};
+    export let div_class;
+
+    const style = `
+        border-radius: 0 8px 8px 0;
+        float: left;
+        position: relative;
+        top: ${settings.top || 0}%;
+        overflow-wrap: break-word;
+        hyphens: manual;
+        inline-size: 70%;
+    `;
+</script>
+
+<div style={style} class={div_class}>
+    <slot></slot>
+</div>
